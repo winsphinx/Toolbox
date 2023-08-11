29983ea0d2da3394b22dab8c3f436f4d673cc653
winsphinx <winsphinx@gmail.com>
2023-08-11 14:40:00 +0800

    use button instead of buttons

--- home.py
+++ home.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from pywebio import start_server
-from pywebio.output import put_buttons, put_markdown
+from pywebio.output import put_button, put_markdown
 from pywebio.session import go_app
 
 from callgroup import Callgroup
@@ -12,8 +12,8 @@ from reversepolarity import Reversepolarity
 def index():
     put_markdown("# 七零八落工具箱")
 
-    put_buttons(["轮选组脚本生成器"], onclick=[lambda: go_app("callgroup")])
-    put_buttons(["反极性脚本生成器"], onclick=[lambda: go_app("reversepolarity")])
+    put_button("轮选组脚本生成器", onclick=lambda: go_app("callgroup"))
+    put_button("反极性脚本生成器", onclick=lambda: go_app("reversepolarity"))
 
 
 def callgroup():
