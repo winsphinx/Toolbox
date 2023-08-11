9b7a265104dc52caddd0a5c4c882b7d64be6d6a9
winsphinx <winsphinx@gmail.com>
2023-08-11 08:40:37 +0800

    fix errors

--- home.py
+++ home.py
@@ -12,8 +12,8 @@ from reversepolarity import Reversepolarity
 def index():
     put_markdown("# 七零八落工具箱")
 
-    put_buttons(["轮选组脚本生成器"], onclick=lambda: go_app("callgroup"))
-    put_buttons(["反极性脚本生成器"], onclick=lambda: go_app("reversepolarity"))
+    put_buttons(["轮选组脚本生成器"], onclick=[lambda: go_app("callgroup")])
+    put_buttons(["反极性脚本生成器"], onclick=[lambda: go_app("reversepolarity")])
 
 
 def callgroup():
